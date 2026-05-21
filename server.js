e990facd2d702416f7ccb68c058f75982483ce06
Priya More <priyamore8446@gmail.com>
2026-05-21 15:19:36 +0530

    Try-catch block is added to handle the error.

--- server.js
+++ server.js
@@ -2,7 +2,13 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port =  8000;
+const port = 8000;
+
+try {
+  require('./index.js');
+} catch (err) {
+  console.error('Failed to start DB connection module:', err);
+}
 
 app.use(express.static(path.join(__dirname, '.')));
 
